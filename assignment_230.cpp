f8bcf0905c96f3f409773b35e96e03d3c8881681
Rogonoc <45210365+Rogonoc@users.noreply.github.com>
2021-12-24 14:05:37 +0100

    Add files via upload

--- assignment_230.cpp
+++ assignment_230.cpp
@@ -1,67 +1,32 @@
 // Libraries
 #include <stdio.h>
-#include <pthread.h>
-#include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <cmath>
 #include <vector>
-#include <signal.h>
-#include <sys/resource.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/syscall.h>
 #include <numeric>
+// Posix related libraries
+#include <pthread.h>
+#include <sys/mman.h> // Library to lock memory
+#include <limits.h>
+#include <sched.h>
 
 // Global parameters
 std::vector<double>     timing(100);
-timer_t                 timerid;
 double                  mean, var, rlt_std, min, max;
-double                  curr_sec, prev_sec, curr_nsec, prev_nsec;
-int                     i, j, k, n, m;
+double                  curr_sec, curr_nsec, prev_sec, prev_nsec;
+int                     i, j, k, n, m, p;
 
 // Function that a thread has to execute
 void *threadFunc(void *pArg) 
 {
-    // Timer parameter
-    struct itimerspec   new_time, old_time;
-
-    // Clock parameter
-    struct timespec     tp;
-
-    // Event parameter
-    struct sigevent     event;
-
-    // Signal parameters
-    sigset_t            set;
-    int                 signum = SIGALRM;
+    // Time parameters
+    struct timespec     tp, ts;
+    ts.tv_nsec = 1000000;               // 1 ms for nanosleep
 
     // Other parameters
     float               test = 0.95;    // Parameter for computational load
-
-    // Create event for timer
-    event.sigev_notify = SIGEV_THREAD_ID;          // Notify thread with a certain ID when event is called
-    event._sigev_un._tid = syscall(__NR_gettid);   // Get the thread ID
-    event.sigev_signo = signum;                    // Call SIGALARM ("signum") when event is started by timer
-
-    // Initialize signal
-    sigemptyset(&set);                             // Initialize signal
-    sigaddset(&set, signum);                       // Associate signum with the set signal
-    sigprocmask(SIG_BLOCK, &set, NULL);            // Initially block signal
-
-    // Create timer
-    if (timer_create(CLOCK_MONOTONIC, &event, &timerid) == -1)
-        perror("timer_create");
-
-    // Timer values
-    new_time.it_interval.tv_sec = 0;
-    new_time.it_interval.tv_nsec = 1000000;    // 1 ms "sleep" initially 
-    new_time.it_value.tv_sec = 0;
-    new_time.it_value.tv_nsec = 1000000;       // 1 ms "sleep" during
-
-    // Set timer
-    if (timer_settime(timerid, 0, &new_time, &old_time) == -1)
-        perror("timer_settime");
 
     // Let the thread perform computations and time it
     for(i = 0; i < 101; i++)
@@ -73,13 +38,17 @@ void *threadFunc(void *pArg)
         }
         /* ---------------------------------------- */
 
-        // Wait (until a signal is raised by the timer)
-        if (sigwait(&set, &signum) == -1)
-            perror("sigwait");
+        // Trying to let a mode switch occur
+        __real_printf("Making syscall\n");
+
+        // Wait (until the sleep is finished)
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL);
 
         // Store timing of loop
         if (clock_gettime(CLOCK_MONOTONIC, &tp) == -1)
-            perror("clock_gettime");
+        {
+            perror("clock gettime");
+        }
 
         /* ---------------- LOG TIME ---------------- */
         curr_sec = tp.tv_sec;
@@ -101,7 +70,7 @@ void *threadFunc(void *pArg)
         prev_sec = curr_sec;
         prev_nsec = curr_nsec;
         /* ------------------------------------------- */
-    } 
+    }
 
     return 0;
 }
@@ -116,7 +85,12 @@ void information()
     }
 
     // Mean
-    mean = std::accumulate(timing.begin(), timing.end(), 0) / timing.size();
+    mean = 0;
+    for (p = 0; p < timing.size(); p++)
+    {
+        mean += timing[p];
+    }
+    mean = mean / timing.size();
 
     // Variance
     var = 0;
@@ -158,10 +132,29 @@ void information()
 int main(int argc, char **argv)
 {
     // Initialize parameters
+    struct sched_param param;
+    pthread_attr_t attr;
     pthread_t thread;
 
-    // Creating a pthread
-    pthread_create(&thread, NULL, threadFunc, NULL);
+    // Lock memory
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+
+    // Initialize pthread attributes (default values)
+    pthread_attr_init(&attr);
+
+    // Set a specific stack size
+    pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN);
+
+    // Set scheduler policy and priority of pthread
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    param.sched_priority = 80;
+    pthread_attr_setschedparam(&attr, &param);
+
+    // User scheduling parameters of attr
+    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+
+    // Create a pthread with specified attributes
+    pthread_create(&thread, &attr, threadFunc, NULL);
 
     // Join the thread and wait until it is done
     pthread_join(thread, NULL);
